4be388f770213cc87e30931d5df40d8516630fbe
Graham Bates <graham@asthecrowflies.co,uk>
2014-12-15 14:20:48 +0000

    Add initial combination of d3 and maps.

--- www/static/scripts/Mapping/Directives/MainMapDirective.js
+++ www/static/scripts/Mapping/Directives/MainMapDirective.js
@@ -12,32 +12,32 @@ Application.Directives.directive('mainmap', function ($state, $stateParams, $fil
         controller: function($scope) {
 
             $scope.mapOptions = {
-                panControl              : false,
+                panControl              : true,
                 zoomControl             : true,
                 mapTypeControl          : true,
                 scaleControl            : false,
                 streetViewControl       : false,
                 overviewMapControl      : false,
-                center                  : new google.maps.LatLng(-34.397, 150.644),
-                zoom                    : 17,
+                center                  : new google.maps.LatLng(34.397, 150.644),
+                zoom                    : 5,
                 styles:[
                     {
                         "stylers": [
                             { "weight": 0.1 },
-                            { "visibility": "off" }
+                            { "visibility": "on" }
                         ]
                     },{
                         "featureType": "administrative.locality",
                         "stylers": [
                             { "visibility": "on" },
-                            { "weight": 4.8 }
+                            { "weight": 0.1 }
 
                         ]
                     },{
                         "featureType": "administrative.neighborhood",
                         "stylers": [
                             { "visibility": "on" },
-                            { "weight": 4.8 }
+                            { "weight": 0.1 }
                         ]
                     },{
                     }
@@ -50,9 +50,47 @@ Application.Directives.directive('mainmap', function ($state, $stateParams, $fil
 
             $scope.overlay      = new google.maps.OverlayView();
 
-            function DCUNetworkGraph(layer) {
+            function Route(layer) {
 
-                this.addNode = function () {
+                this.addLine = function (x, y) {
+
+                    var points = [
+                        [4480, 4200],
+                        [4580, 4400],
+                        [4680, 4100],
+                        [4780, 4300],
+                        [4180, 4300],
+                        [4280, 4100],
+                        [4380, 4400]
+                    ];
+
+                    var line = d3.svg.line()
+                        .tension(0)
+                        .interpolate("cardinal-closed");
+
+                    var svg = layer.datum(points)
+
+                    svg.append("path")
+                        .style("stroke", "#ddd")
+                        .style("stroke-dasharray", "4,4")
+                        .attr("d", line);
+
+                    svg.append("path")
+                        .attr("d", line)
+                        .call(transition);
+
+                    function transition(path) {
+                        path.transition()
+                            .duration(7500)
+                            .attrTween("stroke-dasharray", tweenDash)
+                            .each("end", function() { d3.select(this).call(transition); });
+                    }
+
+                    function tweenDash() {
+                        var l = this.getTotalLength(),
+                            i = d3.interpolateString("0," + l, l + "," + l);
+                        return function(t) { return i(t); };
+                    }
 
                 }
             }
@@ -61,21 +99,13 @@ Application.Directives.directive('mainmap', function ($state, $stateParams, $fil
                 $scope.projection = this.getProjection();
                 var layer  = d3.select(this.getPanes().overlayMouseTarget).append("div").attr("class", "SvgOverlay").append("svg");
 
-                // $scope.graph = new DCUNetworkGraph(layer);
+                 $scope.route = new Route(layer);
             }
 
-            /*This will run every time user pans, zooms etc. */
             $scope.overlay.draw = function() {
+                var pos = $scope.projection.fromLatLngToDivPixel(new google.maps.LatLng(34.397, 150.644));
+                $scope.route.addLine(pos.x+4000, pos.y+4000);
 
-                //$scope.graph.removeallBoundries();
-                //$scope.graph.removeAllNodes();
-                //$scope.graph.removeallLinks();
-
-                //$scope.dcusToShowOnMap.forEach(function(dcu) {
-                //
-                //  $scope.graph.addNode(meter, dcu.id, "meter",   true, meterpos.x, meterpos.y)
-                //
-                //});
 
             };
 
